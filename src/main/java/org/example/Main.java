9331d811ea4456068f3f09fd4ab6b767f067d9f4
H31M44\Student <Student>
2024-03-24 14:52:03 +0700

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,45 @@
 package org.example;
 
+import org.example.polymonial.Element;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
+
+import static org.example.polymonial.Element.parseElement;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+//    public static void main(String[] args) {
+//        Function<Integer, Integer> square = initFilter();
+//        Function<Integer, Integer> half = firstFilter();
+//        Function<Integer, String> toString = finalFilter();
+//        Function<Integer, String> pipeline = square //init
+//                .andThen(half) //first step
+//                .andThen(toString); // second
+//        String result = pipeline.apply(5);
+//        System.out.println(result);
+//    }
+//
+//    private static Function<Integer, String> finalFilter() {
+//        return Object::toString;
+//    }
+//
+//    private static Function<Integer, Integer> firstFilter() {
+//        return s -> s / 2;
+//    }
+//
+//    private static Function<Integer, Integer> initFilter() {
+//        return s -> s * s;
+//    }
     public static void main(String[] args) {
-        Function<Integer, Integer> square = initFilter();
-        Function<Integer, Integer> half = firstFilter();
-        Function<Integer, String> toString = finalFilter();
-        Function<Integer, String> pipeline = square //init
-                .andThen(half) //first step
-                .andThen(toString); // second
-        String result = pipeline.apply(5);
-        System.out.println(result);
+        final String polynomial = "-5x^3 + 9x^6 - 3x + 6x^2 + 8";
+        Function<Integer, List<Element>> init = processParse(polynomial);
+        init.apply(10);
     }
 
-    private static Function<Integer, String> finalFilter() {
-        return Object::toString;
-    }
-
-    private static Function<Integer, Integer> firstFilter() {
-        return s -> s / 2;
-    }
-
-    private static Function<Integer, Integer> initFilter() {
-        return s -> s * s;
+    private static Function<Integer, List<Element>> processParse(String polynomial) {
+        return e -> parseElement(polynomial);
     }
 }
--- src/main/java/org/example/polymonial/Element.java
+++ src/main/java/org/example/polymonial/Element.java
@@ -1,5 +1,75 @@
 package org.example.polymonial;
 
-public class Element {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Element {
+    private String sign;
+    private double coefficient;
+    private int exponent;
+
+    public Element(String sign, double coefficient, int exponent) {
+        this.sign = sign;
+        this.coefficient = coefficient;
+        this.exponent = exponent;
+    }
+
+    public Element() {
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public double getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(double coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public int getExponent() {
+        return exponent;
+    }
+
+    public void setExponent(int exponent) {
+        this.exponent = exponent;
+    }
+
+    @Override
+    public String toString() {
+        return "Element{" +
+                "sign='" + sign + '\'' +
+                ", coefficient=" + coefficient +
+                ", exponent=" + exponent +
+                '}';
+    }
+
+    public static List<Element> parseElement(String element) {
+        List<Element> elements = new ArrayList<>();
+        //String polynomial = "-5x^3 + 9x^6 - 3x + 6x^2 + 8";
+        String[] stringElements = element.split(" ");
+        for (int i = 0; i < stringElements.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(stringElements[i]);
+            } else {
+                System.out.println(stringElements[i]);
+                Element element1 = new Element();
+//                -5x^3
+                if (stringElements[i].length() == 1) {
+                    element1.setSign("stringElements[i].length() == 1"+stringElements[i]);
+                    element1.setCoefficient(Double.parseDouble(stringElements[i]));
+                }
+                elements.add(element1);
+            }
+        }
+
+        System.out.println(elements);
+        return elements;
+    }
 }
